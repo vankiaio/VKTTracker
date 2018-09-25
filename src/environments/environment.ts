9a7fe62d27be89bd77e513e3bd578fab9f592a09
user <liyi@vankia.cn>
2018-09-25 17:40:40 +0800

    VKTTracker v1.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
   votingUrl: 'https://eosportal.io',
   appName: 'VKT Tracker',
   logoUrl: '/assets/logo.png',
-  blockchainUrl: 'http://192.168.0.244:8888',
+  blockchainUrl: 'http://221.122.119.226:8888',
   chainId: 'bebf77db2ae77d4488897e0ab94d6a4267ae5c108752927c8b193c79406b7d9f'
 };
