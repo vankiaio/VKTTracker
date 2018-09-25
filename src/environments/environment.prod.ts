1c39d8a0fd0cbada1526a37bb9a751f3057c5683
user <liyi@vankia.cn>
2018-09-25 19:55:03 +0800

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   walletUrl: 'https://walleteos.com',
   votingUrl: 'https://eosportal.io',
-  appName: 'EOS Tracker',
+  appName: 'VKT Tracker',
   logoUrl: '/assets/logo.png',
   blockchainUrl: 'http://221.122.119.226:8888',
   chainId: 'bebf77db2ae77d4488897e0ab94d6a4267ae5c108752927c8b193c79406b7d9f'
