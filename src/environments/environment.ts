e48792c8c322ba701361e7506e48eb5bf4a67756
vankiaio <liyi@vankia.cn>
2018-11-20 16:26:25 +0800

    v1.1
    v1.1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 
 export const environment = {
   production: false,
-  walletUrl: 'https://walleteos.com',
-  votingUrl: 'https://eosportal.io',
+  walletUrl: 'https://github.com/vankiaio/VKScatterDesktop/releases',
+  votingUrl: 'https://#',
   appName: 'VKT Tracker',
   logoUrl: '/assets/logo.png',
   blockchainUrl: 'http://221.122.119.226:8888',
-  chainId: 'bebf77db2ae77d4488897e0ab94d6a4267ae5c108752927c8b193c79406b7d9f'
+  chainId: 'e17abdaf44e2811b452ea15a0aeb7eff6eab9c5de4452e6fb7b552c5de9ddae7'
 };
